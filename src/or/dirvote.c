0af064163c8ba3322f88fec526aac2eb6e6b4c57
twilsonb <twilsonb mac com>
2014-09-02 20:20:50 +1000

    Silence clang warnings about uninitialised variables by initialising them to defaults, and, where appropriate, asserting valid values

--- src/or/dirvote.c
+++ src/or/dirvote.c
@@ -334,6 +334,9 @@ static int
 compare_vote_rs(const vote_routerstatus_t *a, const vote_routerstatus_t *b)
 {
   int r;
+  tor_assert(a);
+  tor_assert(b);
+  
   if ((r = fast_memcmp(a->status.identity_digest, b->status.identity_digest,
                   DIGEST_LEN)))
     return r;
