5859a1d01dfbd8d37987330a6c22a7744450ed8e
teor <teor2345@gmail.com>
2014-09-29 03:11:30 +1000

    Allow unsafe left shifts in ed25519 using -DUNSAFE_SIGNED_LSHIFT
    Verify the ed25519 changes by diffing:
    cpp -E -DUNSAFE_SIGNED_LSHIFT new_file.c
    cpp -E old_file.c
    Result: whitespace, line number, and (non-significant) bracket changes.

--- src/ext/ed25519/ref10/crypto_int32.h
+++ src/ext/ed25519/ref10/crypto_int32.h
@@ -5,19 +5,25 @@
 
 #include "torint.h"
 #define crypto_int32 int32_t
-#define crypto_uint32 uint32_t
 
 /*
  Stop signed left shifts overflowing
  by using unsigned types for bitwise operations
- */
+*/
 
 #ifndef OVERFLOW_SAFE_SIGNED_LSHIFT
 #define OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, utype, stype) \
   ((stype)((utype)(s) << (utype)(lshift)))
 #endif
 
+#include "crypto_uint32.h"
+
+#ifdef UNSAFE_SIGNED_LSHIFT
+/* the original version of the code */
+#define SHL32(s, lshift) s << lshift
+#else /* #ifndef UNSAFE_SIGNED_LSHIFT */
 #define SHL32(s, lshift) \
   OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint32, crypto_int32)
+#endif /* UNSAFE_SIGNED_LSHIFT */
 
 #endif /* CRYPTO_INT32_H */
--- src/ext/ed25519/ref10/crypto_int64.h
+++ src/ext/ed25519/ref10/crypto_int64.h
@@ -5,19 +5,25 @@
 
 #include "torint.h"
 #define crypto_int64 int64_t
-#define crypto_uint64 uint64_t
 
 /*
  Stop signed left shifts overflowing
  by using unsigned types for bitwise operations
- */
+*/
 
 #ifndef OVERFLOW_SAFE_SIGNED_LSHIFT
 #define OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, utype, stype) \
   ((stype)((utype)(s) << (utype)(lshift)))
 #endif
 
+#include "crypto_uint64.h"
+
+#ifdef UNSAFE_SIGNED_LSHIFT
+/* the original version of the code */
+#define SHL64(s, lshift) s << lshift
+#else /* #ifndef UNSAFE_SIGNED_LSHIFT */
 #define SHL64(s, lshift) \
   OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint64, crypto_int64)
+#endif /* UNSAFE_SIGNED_LSHIFT */
 
 #endif /* CRYPTO_INT64_H */
