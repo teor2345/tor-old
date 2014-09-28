4fcc8d4e55256b96de400ad8cb04be451471853c
teor <teor2345@gmail.com>
2014-09-29 04:20:34 +1000

    Stop ed25519 8-bit signed left shift overflowing
    Add SHL8 in ed25519 similar to SHL32 & 64.
    Standardise usage in ge_scalarmult_base.c for 1 existing fix and 1 new fix.

--- src/ext/ed25519/ref10/crypto_int32.h
+++ src/ext/ed25519/ref10/crypto_int32.h
@@ -26,4 +26,17 @@
   OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint32, crypto_int32)
 #endif /* UNSAFE_SIGNED_LSHIFT */
 
+/* And for 8 bit types */
+
+#define crypto_int8 int8_t
+#define crypto_uint8 uint8_t
+
+#ifdef UNSAFE_SIGNED_LSHIFT
+/* the original version of the code */
+#define SHL8(s, lshift) s << lshift
+#else /* #ifndef UNSAFE_SIGNED_LSHIFT */
+#define SHL8(s, lshift) \
+  OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint8, crypto_int8)
+#endif /* UNSAFE_SIGNED_LSHIFT */
+
 #endif /* CRYPTO_INT32_H */
