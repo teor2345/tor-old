db280c60a469db567d1f8830170be78eaadac7f4
teor <teor2345@gmail.com>
2014-10-22 14:23:17 +1100

    Xcode Project - Universal Binaries
    Create architecture-specific includes for curve25519-donna[-64].c.
    Add dependencies between the various components.
    
    The project uses configure to generate orconfig.h for x86_64 & i386,
    make to build micro-revision.i, common_sha1.i, and or_sha1.i, and
    Xcode to build universal binaries for tor, bench, and test*.
    
    Known Issues:
    HomeBrew won't work until I move configure arguments out of the project
    (into an editable script?) and source header & library paths from
    configure/Makefile.
    test can't find test-bt-cl, test-ntor-cl, and test-child due to path issues
    (these tests currently fail).
    There is no README or changes file yet.

---
+++ contrib/osxbuild/curve25519-donna-arch.c
@@ -0,0 +1,11 @@
+
+/* define USE_CURVE25519_DONNA_C64 to 0 to avoid using c64 code on x86_64 */
+#ifndef USE_CURVE25519_DONNA_C64
+#define USE_CURVE25519_DONNA_C64 1
+#endif
+
+#if defined(__x86_64__) && USE_CURVE25519_DONNA_C64
+#include "../../src/ext/curve25519_donna/curve25519-donna-c64.c"
+#else /* __i386__ || !USE_CURVE25519_DONNA_C64 */
+#include "../../src/ext/curve25519_donna/curve25519-donna.c"
+#endif /* __x86_64__ && USE_CURVE25519_DONNA_C64 */
