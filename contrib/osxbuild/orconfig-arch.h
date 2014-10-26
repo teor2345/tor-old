0f6093352d48a477ee28ff5acffd3e7d46194a18
teor <teor2345@gmail.com>
2014-10-26 16:03:44 +1100

    Add initial copies of generated OS X files, tune warnings

--- contrib/osxbuild/orconfig-arch.h
+++ contrib/osxbuild/orconfig-arch.h
@@ -1,3 +1,9 @@
+/** This file is generated based on the build architecture list,
+ * then copied over tor/orconfig.h before building. */
+
+/** Xcode expects architecture-specific code to be conditionalised,
+ * rather than being in different files. It doesn't have per-architecture
+ * file -> target memberships, so we fake it here. */
 
 #ifdef __x86_64__
 #include "contrib/osxbuild/orconfig-x86_64.h"
