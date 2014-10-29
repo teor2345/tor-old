3be7bfde44fe7152f2d1d179b90e35bd2145bfa7
Matthew Finkel <Matthew.Finkel@gmail.com>
2014-10-29 00:29:48 +0000

    Client should check if dir server has open dir port or handles tunnelled requests
    Final piece of prop 237. Closes 12538.

--- src/or/nodelist.c
+++ src/or/nodelist.c
@@ -644,12 +644,16 @@ node_is_named(const node_t *node)
 int
 node_is_dir(const node_t *node)
 {
-  if (node->rs)
-    return node->rs->dir_port != 0;
-  else if (node->ri)
-    return node->ri->dir_port != 0;
-  else
+  if (node->rs) {
+    routerstatus_t * rs = node->rs;
+    return rs->is_v2_dir || rs->dir_port > 0;
+  } else if (node->ri) {
+    routerinfo_t * ri = node->ri;
+    return ri->supports_tunnelled_dir_requests ||
+           ri->dir_port > 0;
+  } else {
     return 0;
+  }
 }
 
 /** Return true iff <b>node</b> has either kind of usable descriptor -- that
