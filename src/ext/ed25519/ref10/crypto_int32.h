506210d8c5fc8564346edaf764e69e21163b8334
teor <teor2345@gmail.com>
2014-12-20 23:02:56 +1100

    Merge curve25519_donna 32-bit changes

--- src/ext/ed25519/ref10/crypto_int32.h
+++ src/ext/ed25519/ref10/crypto_int32.h
@@ -10,11 +10,39 @@
 /*
  Stop signed left shifts overflowing
  by using unsigned types for 8-bit & 32-bit bitwise operations
- */
+*/
 
-#define SHL8(s, lshift) \
-  OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, unsigned char, signed char)
+#ifndef OVERFLOW_SAFE_SIGNED_LSHIFT
+#define OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, utype, stype) \
+  ((stype)((utype)(s) << (utype)(lshift)))
+#endif
+
 #define SHL32(s, lshift) \
   OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint32, crypto_int32)
+#define SHL8(s, lshift) \
+  OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, unsigned char, signed char)
+
+#include "crypto_uint32.h"
+
+#ifdef UNSAFE_SIGNED_LSHIFT
+/* the original version of the code */
+#define SHL32(s, lshift) s << lshift
+#else /* #ifndef UNSAFE_SIGNED_LSHIFT */
+#define SHL32(s, lshift) \
+  OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint32, crypto_int32)
+#endif /* UNSAFE_SIGNED_LSHIFT */
+
+/* And for 8 bit types */
+
+#define crypto_int8 int8_t
+#define crypto_uint8 uint8_t
+
+#ifdef UNSAFE_SIGNED_LSHIFT
+/* the original version of the code */
+#define SHL8(s, lshift) s << lshift
+#else /* #ifndef UNSAFE_SIGNED_LSHIFT */
+#define SHL8(s, lshift) \
+  OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint8, crypto_int8)
+#endif /* UNSAFE_SIGNED_LSHIFT */
 
 #endif /* CRYPTO_INT32_H */
--- src/ext/ed25519/ref10/crypto_int64.h
+++ src/ext/ed25519/ref10/crypto_int64.h
@@ -10,9 +10,21 @@
 /*
  Stop signed left shifts overflowing
  by using unsigned types for 64-bit bitwise operations
- */
+*/
 
+#ifndef OVERFLOW_SAFE_SIGNED_LSHIFT
+#define OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, utype, stype) \
+  ((stype)((utype)(s) << (utype)(lshift)))
+#endif
+
+#include "crypto_uint64.h"
+
+#ifdef UNSAFE_SIGNED_LSHIFT
+/* the original version of the code */
+#define SHL64(s, lshift) s << lshift
+#else /* #ifndef UNSAFE_SIGNED_LSHIFT */
 #define SHL64(s, lshift) \
   OVERFLOW_SAFE_SIGNED_LSHIFT(s, lshift, crypto_uint64, crypto_int64)
+#endif /* UNSAFE_SIGNED_LSHIFT */
 
 #endif /* CRYPTO_INT64_H */
