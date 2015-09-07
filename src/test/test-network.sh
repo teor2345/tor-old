fd9fb9600096e57e855686b9019175617fd55351
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2015-09-08 03:28:21 +1000

    Whitespace-only changes to src/test/test-network.sh
    Intent while and if blocks
    Standardise file to 2-space tab stops
    Add newlines for readability

--- src/test/test-network.sh
+++ src/test/test-network.sh
@@ -67,16 +67,16 @@ CHUTNEY_NETWORK=networks/$NETWORK_FLAVOUR
 myname=$(basename $0)
 
 [ -n "$CHUTNEY_PATH" ] || {
-    echo "$myname: \$CHUTNEY_PATH not set, trying $TOR_DIR/../chutney"
-    CHUTNEY_PATH="$TOR_DIR/../chutney"
+  echo "$myname: \$CHUTNEY_PATH not set, trying $TOR_DIR/../chutney"
+  CHUTNEY_PATH="$TOR_DIR/../chutney"
 }
 
 [ -d "$CHUTNEY_PATH" ] && [ -x "$CHUTNEY_PATH/chutney" ] || {
-    echo "$myname: missing 'chutney' in CHUTNEY_PATH ($CHUTNEY_PATH)"
-    echo "$myname: Get chutney: git clone https://git.torproject.org/\
+  echo "$myname: missing 'chutney' in CHUTNEY_PATH ($CHUTNEY_PATH)"
+  echo "$myname: Get chutney: git clone https://git.torproject.org/\
 chutney.git"
-    echo "$myname: Set \$CHUTNEY_PATH to a non-standard location: export CHUTNEY_PATH=\`pwd\`/chutney"
-    exit 1
+  echo "$myname: Set \$CHUTNEY_PATH to a non-standard location: export CHUTNEY_PATH=\`pwd\`/chutney"
+  exit 1
 }
 
 cd "$CHUTNEY_PATH"
@@ -92,51 +92,59 @@ export CHUTNEY_TOR_GENCERT="${TOR_DIR}/src/tools/${tor_gencert_name}"
 CHUTNEY_RECONFIGURES=${CHUTNEY_RECONFIGURES:-2}
 c=0
 while [ $c -lt $CHUTNEY_RECONFIGURES ]; do
-echo "$myname: Configuring and starting network..."
-# bootstrap-network.sh exits with the result of chutney status,
-# which is -1 on failure
-./tools/bootstrap-network.sh $NETWORK_FLAVOUR
-BOOTSTRAP_EXIT_STATUS=$?
-if [ $BOOTSTRAP_EXIT_STATUS -ne 0 ]; then
+  echo "$myname: Configuring and starting network..."
+  # bootstrap-network.sh exits with the result of chutney status,
+  # which is -1 on failure
+  ./tools/bootstrap-network.sh $NETWORK_FLAVOUR
+  BOOTSTRAP_EXIT_STATUS=$?
+
+  if [ $BOOTSTRAP_EXIT_STATUS -ne 0 ]; then
     echo "$myname: Retrying configuring and starting network..."
     c=$(expr $c + 1)
     continue
-fi
+  fi
 
-# Sleep some, waiting for the network to bootstrap.
-# TODO: Add chutney command 'bootstrap-status' and use that instead.
-BOOTSTRAP_TIME=${BOOTSTRAP_TIME:-20}
-MAX_BOOTSTRAP_TIME=${MAX_BOOTSTRAP_TIME:-60}
-# Trying every 5 seconds seems reasonable, most systems will take 1-2 tries
-# as typical bootstrap times are 20-25 seconds
-VERIFY_INTERVAL=5
-$ECHO_N "$myname: sleeping for $BOOTSTRAP_TIME seconds"
-n=0; while [ $n -le $MAX_BOOTSTRAP_TIME ]; do
-    sleep 1; n=$(expr $n + 1); $ECHO_N .
+  # Sleep some, waiting for the network to bootstrap.
+  # TODO: Add chutney command 'bootstrap-status' and use that instead.
+  BOOTSTRAP_TIME=${BOOTSTRAP_TIME:-20}
+  MAX_BOOTSTRAP_TIME=${MAX_BOOTSTRAP_TIME:-60}
+  # Trying every 5 seconds seems reasonable, most systems will take 1-2 tries
+  # as typical bootstrap times are 20-25 seconds
+  VERIFY_INTERVAL=5
+  $ECHO_N "$myname: sleeping for $BOOTSTRAP_TIME seconds"
+  n=0
+  while [ $n -le $MAX_BOOTSTRAP_TIME ]; do
+    sleep 1;
+    n=$(expr $n + 1);
+    $ECHO_N .
+
     if [ $n -ge $BOOTSTRAP_TIME ]; then
-        echo ""
-./chutney verify $CHUTNEY_NETWORK
-VERIFY_EXIT_STATUS=$?
-        if [ $VERIFY_EXIT_STATUS -eq 0 ]; then
-# work around a bug/feature in make -j2 (or more)
-# where make hangs if any child processes are still alive
-./chutney stop $CHUTNEY_NETWORK
-exit $VERIFY_EXIT_STATUS
-        else
-            $ECHO_N "$myname: sleeping for $VERIFY_INTERVAL seconds"
-            BOOTSTRAP_TIME=$(expr $BOOTSTRAP_TIME + $VERIFY_INTERVAL);
-        fi
+      echo ""
+      ./chutney verify $CHUTNEY_NETWORK
+      VERIFY_EXIT_STATUS=$?
+
+      if [ $VERIFY_EXIT_STATUS -eq 0 ]; then
+        # work around a bug/feature in make -j2 (or more)
+        # where make hangs if any child processes are still alive
+        ./chutney stop $CHUTNEY_NETWORK
+        exit $VERIFY_EXIT_STATUS
+      else
+        $ECHO_N "$myname: sleeping for $VERIFY_INTERVAL seconds"
+        BOOTSTRAP_TIME=$(expr $BOOTSTRAP_TIME + $VERIFY_INTERVAL);
+      fi
     fi
-done; # while [ $n -le $MAX_BOOTSTRAP_TIME ]
-echo "" 
-# at this point, we have failed by exceeding the maximum bootstrap time
-echo "Maximum bootstrap time ($MAX_BOOTSTRAP_TIME) exceeded."
-echo "Use --max-time N or MAX_BOOTSTRAP_TIME=N to add more time."
-# work around a bug/feature in make -j2 (or more)
-# where make hangs if any child processes are still alive
-./chutney stop $CHUTNEY_NETWORK
-exit $VERIFY_EXIT_STATUS
+  done # while [ $n -le $MAX_BOOTSTRAP_TIME ]
+  echo "" 
+
+  # at this point, we have failed by exceeding the maximum bootstrap time
+  echo "Maximum bootstrap time ($MAX_BOOTSTRAP_TIME) exceeded."
+  echo "Use --max-time N or MAX_BOOTSTRAP_TIME=N to add more time."
+  # work around a bug/feature in make -j2 (or more)
+  # where make hangs if any child processes are still alive
+  ./chutney stop $CHUTNEY_NETWORK
+  exit $VERIFY_EXIT_STATUS
 done # while [ $c -lt "$try_chutney_configure" ]
+
 # at this point, we have failed by exceeding the configure retries
 echo "Maximum number of configuration tries ($CHUTNEY_RECONFIGURES) exceeded."
 echo "Use --configure N or CHUTNEY_RECONFIGURES=N to add more attempts."
