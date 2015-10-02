404c9313c0182209608440ce17028b6c24afcafe
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2015-10-02 09:47:10 +0200

    fixup in our first cut, don't filter out too many relays

--- scripts/maint/updateFallbackDirs.py
+++ scripts/maint/updateFallbackDirs.py
@@ -84,16 +84,16 @@ STRICT_FALLBACK_WEIGHTS = False
 # * elimination of low weight relays
 # * consensus weight changes
 # * fallback directory losses over time
-# A relay weighted at 1 in 20 fallbacks will see about 5% of clients that
+# A relay weighted at 1 in 5 fallbacks will see about 20% of clients that
 # use the fallback directories.
-TARGET_MAX_WEIGHT_FRACTION = 1/20.0
+TARGET_MAX_WEIGHT_FRACTION = 1/5.0
 REWEIGHTING_FUDGE_FACTOR = 0.8
 MAX_WEIGHT_FRACTION = TARGET_MAX_WEIGHT_FRACTION * REWEIGHTING_FUDGE_FACTOR
 # If a single fallback's weight is too low, it's pointless adding it.
 # (Final weights may be slightly higher than this, due to low weight relays
 # being excluded.)
-# A relay weighted at 1 in 2000 fallbacks will see about 0.05% of clients.
-MIN_WEIGHT_FRACTION = 1/2000.0
+# A relay weighted at 1 in 100000 fallbacks will see about 0.0001% of clients.
+MIN_WEIGHT_FRACTION = 1/100000.0
 
 ## Other Configuration Parameters
 
