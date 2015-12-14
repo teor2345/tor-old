c2b21db12372bdc533a50988aa958fdae2422c3f
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2015-12-14 20:20:52 +1100

    Apply the 6c443e987d fix to router_pick_directory_server_impl
    Also remove redundant "n_busy = 0;" conditional on !n_busy.

--- src/or/routerlist.c
+++ src/or/routerlist.c
@@ -1580,7 +1580,6 @@ router_pick_directory_server_impl(dirinfo_type_t type, int flags,
      * not set, try again without excluding nodes. */
     try_excluding = 0;
     n_excluded = 0;
-    n_busy = 0;
     goto retry_without_exclude;
   }
 
@@ -1718,21 +1717,22 @@ router_pick_trusteddirserver_impl(const smartlist_t *sourcelist,
       result = &selection->fake_status;
   }
 
-  if (n_busy_out)
-    *n_busy_out = n_busy;
-
   smartlist_free(direct);
   smartlist_free(tunnel);
   smartlist_free(overloaded_direct);
   smartlist_free(overloaded_tunnel);
 
-  if (result == NULL && try_excluding && !options->StrictNodes && n_excluded) {
+  if (result == NULL && try_excluding && !options->StrictNodes && n_excluded
+      && !n_busy) {
     /* If we got no result, and we are excluding nodes, and StrictNodes is
      * not set, try again without excluding nodes. */
     try_excluding = 0;
     n_excluded = 0;
     goto retry_without_exclude;
   }
+
+  if (n_busy_out)
+    *n_busy_out = n_busy;
 
   return result;
 }
