4eec5517007c171758fc1351eab8fb24d4bd6086
David Goulet <dgoulet@ev0ke.net>
2015-12-16 16:39:55 +0100

    fixup! Client should check if dir server has open dir port or handles tunnelled requests

--- src/or/nodelist.c
+++ src/or/nodelist.c
@@ -646,11 +646,14 @@ node_is_dir(const node_t *node)
 {
   if (node->rs) {
     routerstatus_t * rs = node->rs;
-    return rs->is_v2_dir || rs->dir_port > 0;
+    /* This is true if supports_tunnelled_dir_requests is true which
+     * indicates that we support directory request tunnelled or through the
+     * DirPort. */
+    return rs->is_v2_dir;
   } else if (node->ri) {
     routerinfo_t * ri = node->ri;
-    return ri->supports_tunnelled_dir_requests ||
-           ri->dir_port > 0;
+    /* Both tunnelled request is supported or DirPort is set. */
+    return ri->supports_tunnelled_dir_requests;
   } else {
     return 0;
   }
