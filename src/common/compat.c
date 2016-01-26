837cae24bda9fda63df87a4886b83cd6d0205542
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2016-01-26 13:45:01 +1100

    Correct further grammatical errors in tor comments
    Avoid using a pronoun where it makes comments unclear.
    Avoid using gender for things that don't have it.
    Avoid assigning gender to people unnecessarily.

--- src/common/compat.c
+++ src/common/compat.c
@@ -717,7 +717,7 @@ strtok_helper(char *cp, const char *sep)
 }
 
 /** Implementation of strtok_r for platforms whose coders haven't figured out
- * how to write one.  Hey guys!  You can use this code here for free! */
+ * how to write one.  You can use this code here for free! */
 char *
 tor_strtok_r_impl(char *str, const char *sep, char **lasts)
 {
