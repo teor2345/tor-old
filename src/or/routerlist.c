973b24586ccdc538baef00f7995a1e99a22c1f3a
teor (Tim Wilson-Brown) <teor2345@gmail.com>
2016-05-10 11:14:09 -0400

    Revert "fixup! Switch between fallback and authority when auth cert fetch fails"
    This reverts commit bea081976052b025983035620b43ca24368a0c89.

--- src/or/routerlist.c
+++ src/or/routerlist.c
@@ -911,7 +911,7 @@ authority_certs_fetch_missing(networkstatus_t *status, time_t now)
     } SMARTLIST_FOREACH_END(d);
 
     if (smartlist_len(fps) > 1) {
-      static int want_auth = 1;
+      static int want_auth = 0;
       resource = smartlist_join_strings(fps, "", 0, NULL);
       directory_get_from_dirserver(DIR_PURPOSE_FETCH_CERTIFICATE, 0,
                                    resource, PDS_RETRY_IF_NO_SERVERS,
@@ -960,7 +960,7 @@ authority_certs_fetch_missing(networkstatus_t *status, time_t now)
     } SMARTLIST_FOREACH_END(d);
 
     if (smartlist_len(fp_pairs) > 1) {
-      static int want_auth = 1;
+      static int want_auth = 0;
       resource = smartlist_join_strings(fp_pairs, "", 0, NULL);
       directory_get_from_dirserver(DIR_PURPOSE_FETCH_CERTIFICATE, 0,
                                    resource, PDS_RETRY_IF_NO_SERVERS,
