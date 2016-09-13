78340c225f1e41dc6d0e10a3cf099f69100bfb0d
teor <teor2345@gmail.com>
2016-09-13 11:41:56 +1000

    fixup! Refactor the hidden service code to use rend_service_path

--- src/or/rendservice.c
+++ src/or/rendservice.c
@@ -960,17 +960,18 @@ rend_service_update_descriptor(rend_service_t *service)
 
 /* Allocate and return a string containing the path to file_name in
  * service->directory. Asserts that service has a directory.
+ * This function will never return NULL.
  * The caller must free this path. */
 static char *
 rend_service_path(const rend_service_t *service, const char *file_name)
 {
-  char *file_path;
+  char *file_path = NULL;
 
   tor_assert(service->directory);
 
+  /* Can never fail: asserts rather than leaving file_path NULL. */
   tor_asprintf(&file_path, "%s%s%s",
                service->directory, PATH_SEPARATOR, file_name);
-  tor_assert(file_path);
 
   return file_path;
 }
