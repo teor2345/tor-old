b07f2f97fe7975525f7688800d3e62e3facf1150
teor <teor2345@gmail.com>
2017-09-14 13:17:22 +1000

    fixup! Stop revealing information about the low bits of HS statistics

--- src/or/rephist.c
+++ src/or/rephist.c
@@ -3159,27 +3159,54 @@ rep_hist_format_hs_stats(time_t now)
   int64_t obfuscated_onions_seen;
 
   /* When adding noise to statistics, we must add noise, and then truncate,
-   * and then bin. Any operations outside this order may be unsafe, and
-   * must be documented. */
+   * and then bin. Doing these operations outside this order, or doing any
+   * operations may be unsafe, and must be documented. */
 
   uint64_t cells_seen = hs_stats->rp_relay_cells_seen;
-  /* Clamping destroys most of the bits in large signals, and passes small
-   * signals through.
-   * See the comments in add_laplace_noise() for why we think this is safe. */
+  /* Clamping treats large values as if they are infinite.
+   * This is safe as long as add_laplace_noise() preserves infinities. */
   int64_t clamped_cells_seen = MIN(cells_seen, INT64_MAX);
+  /* Noise must be sampled and added using safe operations. */
   int64_t noised_cells_seen = add_laplace_noise(clamped_cells_seen,
                                                     crypto_rand_double(),
                                                     REND_CELLS_DELTA_F,
                                                     REND_CELLS_EPSILON);
-  /* Destroying the low bits of the noised signal is always safe, as long as
-   * it is done to every value, and as the final operation. */
+  /* Destroying the low bits of the separate signal or noise is not safe.
+   * But destroying the low bits of the noised signal is safe.
+   * This is also safe for infinities and nearby values, because it groups them
+   * together. But it changes the output value for infinite signal or noise,
+   * so it must be the last operation. (Any further operations won't preserve
+   * infinities.) */
   obfuscated_cells_seen = round_int64_to_next_multiple_of(noised_cells_seen,
                                                           REND_CELLS_BIN_SIZE);
 
-  /* digestmap_size clamps its unsigned value to an int. But we do this
-   * redundant clamping in case the underlying implementation ever changes.
-   * The safety rationales for onions_seen are the same as for cells_seen. */
+
+  /* The safety rationales for onions_seen are the same as for cells_seen,
+   * except where documented below. */
+
+  /* Casting the returned int to uint64_t restores the original unsigned
+   * value in the struct. */
   uint64_t onions_seen = digestmap_size(hs_stats->onions_seen_this_period);
+
+  /* digestmap_size uses native integer sizes. add_laplace_noise() only
+   * preserves INT64_MAX, so this could be unsafe on 32-bit architectures.
+   * But they shouldn't have enough RAM to trigger this case anyway.
+   * Out of scope, because this is a rare case:
+   *  - is it ok that different machines stop counting at different maxima?
+   *  - what about 32-bit machines where counts are just under INT32_MAX?
+   *    does this reveal information about the noise (and the signal)?
+   */
+#if SIZEOF_INT < SIZEOF_INT64_T
+  if (BUG((unsigned)digestmap_size(hs_stats->onions_seen_this_period) >=
+          (unsigned)INT_MAX)) {
+    /* Don't report any HS stats in the extrainfo for this period */
+    return strdup("");
+  }
+#endif
+
+  /* Clamp to INT64_MAX, in case the integer type returned by digestmap_size
+   * ever changes to size_t. (This ensures safety on 64-bit platforms: the
+   * 32-bit check is above.) */
   int64_t clamped_onions_seen = MIN(onions_seen, INT64_MAX);
   int64_t noised_onions_seen = add_laplace_noise(clamped_onions_seen,
                                                  crypto_rand_double(),
