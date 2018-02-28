874a73cc693e6e89350160ca84d0a51e58a21929
teor <teor2345@gmail.com>
2018-02-28 23:10:46 +1100

    squash! rust: Add a crypto_rand module containing get_random_f64_sign()
    Add more tests for F64_INTEGER_MAX, and make using it easier.

--- src/rust/crypto_rand_f64/tests/crypto_rand_f64_sign_distribution.rs
+++ src/rust/crypto_rand_f64/tests/crypto_rand_f64_sign_distribution.rs
@@ -15,26 +15,45 @@ use std::f64::*;
 
 use crypto_rand_f64::*;
 
-/// Use the same number of iterations as test_crypto.c
-const RANDOM_TEST_ITERATIONS : u64 = 1000;
+/// Aim for a similar failure probability to test_crypto.c
+/// We use an odd power of two because it produces exact binomial results in
+/// `test_rand_f64_sign_even_distribution()`
+const RANDOM_TEST_ITERATIONS : u64 = 512;
 
-/// The largest f64 that has integer precision
-const F64_INTEGER_MAX : f64 = ((1 as u64) << MANTISSA_DIGITS) as f64;
+/// The largest `f64` that has integer precision
+/// The `f64` cast is safe, because this value has integer precision
+const F64_INTEGER_MAX_AS_U64 : u64 = 1 << MANTISSA_DIGITS;
+const F64_INTEGER_MAX : f64 = F64_INTEGER_MAX_AS_U64 as f64;
 
-/// Integers less than F64_INTEGER_MAX are exactly representable in f64
+/// Integers equal to `F64_INTEGER_MAX` are exactly representable in `f64`
 #[test]
-fn test_f64_integer_max_decrement_exact() {
-    // This is a deliberate floating point precision test
-    assert!(F64_INTEGER_MAX - 1.0 != F64_INTEGER_MAX);
+fn test_f64_integer_max_exact() {
+    // These tests are deliberate floating point precision tests
+    assert!(F64_INTEGER_MAX as u64 == F64_INTEGER_MAX_AS_U64);
+    assert!(F64_INTEGER_MAX == F64_INTEGER_MAX_AS_U64 as f64);
 }
 
-/// Some integers greater than F64_INTEGER_MAX are not exactly representable
-/// in f64.
+/// Integers less than `F64_INTEGER_MAX` are exactly representable in `f64`
+#[test]
+fn test_f64_integer_max_decrement_exact() {
+    // These tests are deliberate floating point precision tests
+    assert!(F64_INTEGER_MAX - 1.0 != F64_INTEGER_MAX);
+    assert!((F64_INTEGER_MAX - 1.0) as u64 == F64_INTEGER_MAX_AS_U64 - 1);
+    assert!((F64_INTEGER_MAX as u64) - 1 == F64_INTEGER_MAX_AS_U64 - 1);
+    assert!(F64_INTEGER_MAX - 1.0 == (F64_INTEGER_MAX_AS_U64 - 1) as f64);
+    assert!(F64_INTEGER_MAX - 1.0 == (F64_INTEGER_MAX_AS_U64 as f64) - 1.0);
+}
+
+/// Some integers greater than `F64_INTEGER_MAX` are not exactly representable
+/// in `f64`.
 #[test]
 fn test_f64_integer_max_increment_inexact() {
-    // This is a deliberate floating point precision test
-    // This test may fail if rustc uses extended precision floats.
+    // These tests are deliberate floating point precision tests
+    // These tests may fail if rustc uses extended precision floats
     assert!(F64_INTEGER_MAX + 1.0 == F64_INTEGER_MAX);
+    assert!((F64_INTEGER_MAX + 1.0) as u64 == F64_INTEGER_MAX_AS_U64);
+    assert!((F64_INTEGER_MAX as u64) + 1 == F64_INTEGER_MAX_AS_U64 + 1);
+    assert!(F64_INTEGER_MAX + 1.0 == F64_INTEGER_MAX_AS_U64 as f64);
 }
 
 /// Do both outputs of `get_rand_f64_sign()` occur at all?
@@ -50,20 +69,19 @@ fn test_rand_f64_sign_both_values_occur() {
             got_negative = true;
         }
     }
-    // These fail with probability 1/2^1000
+    // These fail with probability 1/2^RANDOM_TEST_ITERATIONS
     assert!(got_positive);
     assert!(got_negative);
 }
 
 /// Do both outputs of `get_rand_f64_sign()` occur with the expected
-/// distribution?
+/// distribution after `RANDOM_TEST_ITERATIONS` trials?
 #[test]
 fn test_rand_f64_sign_even_distribution() {
     let mut accumulator = 0.0;
     for _ in 0..RANDOM_TEST_ITERATIONS {
         // Check RANDOM_TEST_ITERATIONS is not too large
-        // The u64 cast is safe, because F64_INTEGER_MAX has integer precision
-        assert!(RANDOM_TEST_ITERATIONS <= (F64_INTEGER_MAX as u64));
+        assert!(RANDOM_TEST_ITERATIONS <= F64_INTEGER_MAX_AS_U64);
         // This floating-point addition is exact, and does not suffer from
         // catastropic cancellation, as long as RANDOM_TEST_ITERATIONS is not
         // too large
@@ -85,7 +103,7 @@ fn test_rand_f64_sign_even_distribution() {
     let ten_sigma = 10.0 * stdev;
     
     // check that the f64 cast below is safe
-    assert!(RANDOM_TEST_ITERATIONS <= (F64_INTEGER_MAX as u64));
+    assert!(RANDOM_TEST_ITERATIONS <= F64_INTEGER_MAX_AS_U64);
     // check that we haven't set ten_sigma too high
     // (otherwise, this test will never fail)
     assert!(ten_sigma < (RANDOM_TEST_ITERATIONS as f64));
